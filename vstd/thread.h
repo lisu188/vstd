cbf40b44e6cb7b370e30a47ecf464d668c7d4ef7
Lis, Andrzej <andrzej.lis3@gmail.com>
2015-12-25 21:27:17 +0100

    sperate handlers

--- vstd/thread.h
+++ vstd/thread.h
@@ -6,39 +6,43 @@
 #include "threadpool.h"
 
 namespace vstd {
+    extern std::function<void(std::function<void()>)> get_call_later_handler();
+    extern std::function<void(std::function<void()>)> get_call_async_handler();
+    extern std::function<void(std::function<void()>)> get_call_later_block_handler();
+    extern std::function<void(std::function<void()>)> get_wait_until_handler();
+
+    std::function<void(std::function<void()>)> call_later_handler=get_call_later_handler();
+    std::function<void(std::function<void()>)> call_async_handler=get_call_async_handler();
+    std::function<void(std::function<void()>)> call_later_block_handler=get_call_later_block_handler();
+    std::function<void(std::function<bool()>)> wait_until_handler=get_wait_until_handler();
+
     template<typename Function, typename... Arguments>
-    force_inline void call_later ( Function target, Arguments... args ) {
-       QApplication::postEvent ( CInvocationHandler::instance(),
-                                 new CInvocationEvent ( vstd::bind ( target, args... ) ) );
+    force_inline void call_later(Function target, Arguments... args) {
+        call_later_handler(vstd::bind(target, args...));
     }
 
     template<typename Function, typename... Arguments>
-    force_inline void call_async ( Function target, Arguments... args ) {
-        static std::shared_ptr<vstd::thread_pool<16>> pool = std::make_shared<vstd::thread_pool<16>>()->start();
-        pool->execute ( target, args... );
+    force_inline void call_async(Function target, Arguments... args) {
+        call_async_handler(vstd::bind(target, args...));
     }
 
     template<typename Function, typename... Arguments>
-    force_inline void call_later_block ( Function target, Arguments... args ) {
-        QApplication::sendEvent ( CInvocationHandler::instance(),
-                                  new CInvocationEvent ( vstd::bind ( target, args... ) ) );
+    force_inline void call_later_block(Function target, Arguments... args) {
+        call_later_block_handler(vstd::bind(target, args...));
     }
+
 
     template<typename Predicate>
-    force_inline void wait_until ( Predicate pred ) {
-        call_later_block ( [pred]() {
-            while ( !pred() ) {
-                QApplication::processEvents ( QEventLoop::WaitForMoreEvents );
-            }
-        } );
+    force_inline void wait_until(Predicate pred) {
+        wait_until_handler(pred);
     }
 
     template<typename Predicate, typename Function, typename... Arguments>
-    force_inline void call_when ( Predicate pred, Function func, Arguments... params ) {
-        auto bind = vstd::bind ( func, params... );
-        call_later ( [pred, bind]() {
-            wait_until ( pred );
-            call_later ( bind );
-        } );
+    force_inline void call_when(Predicate pred, Function func, Arguments... params) {
+        auto bind = vstd::bind(func, params...);
+        call_later([pred, bind]() {
+            wait_until(pred);
+            call_later(bind);
+        });
     }
 }
