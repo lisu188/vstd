cae98ef023710ebabfa62e2bc2f28fa73b3ef97c
Lis, Andrzej <andrzej.lis3@gmail.com>
2016-01-13 16:20:42 +0100

    PY_UNSAFE

--- vdefines.h
+++ vdefines.h
@@ -8,6 +8,7 @@
 
 #ifdef Py_PYTHON_H
 #define PY_SAFE(x) try{x}catch(...){vstd::logger::debug();PyErr_Print();PyErr_Clear();}
+#define PY_UNSAFE(x) try{x}catch(...){vstd::logger::debug();PyErr_Print();PyErr_Clear();throw;}
 #define PY_SAFE_RET(x) try{x}catch(...){vstd::logger::debug();PyErr_Print();PyErr_Clear();return nullptr;}
 #define PY_SAFE_RET_VAL(x,r) try{x}catch(...){vstd::logger::debug();PyErr_Print();PyErr_Clear();return r;}
 #endif
