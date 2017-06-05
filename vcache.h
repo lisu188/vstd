078df16165af2a5c7bb2bd1cad3c0537b62854b2
Andrzej Lis <andrzej.lis3@gmail.com>
2017-06-05 10:34:54 +0200

    implement cache

--- vcache.h
+++ vcache.h
@@ -1,15 +1,15 @@
 #pragma once
 
 namespace vstd {
-    template<typename key, typename val, int timeout, val(*gen)()>
+    template<typename key, typename val, val(*get_next)(), int(*get_ttl)()>
     class cache {
         std::unordered_map<key, std::pair<val, int>> _values;
     public:
         val get(key k, int time) {
             auto it = _values.find(k);
             if (it == _values.end()) {
-                val ret = gen();
-                _values.insert(std::make_pair(k, std::make_pair(ret, time + timeout)));
+                val ret = get_next();
+                _values.insert(std::make_pair(k, std::make_pair(ret, time + get_ttl())));
                 return ret;
             }
             if ((*it).second.second < time) {
