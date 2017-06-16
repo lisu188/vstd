15d12b41622f3b36273770ac5fb3fd6a048148ce
Andrzej Lis <andrzej.lis3@gmail.com>
2017-06-16 21:47:19 +0200

    desynchronize all animations

--- vcache.h
+++ vcache.h
@@ -19,4 +19,23 @@ namespace vstd {
             return (*it).second.first;
         }
     };
+
+    template<typename key, typename val, int(*get_ttl)()>
+    class cache2 {
+        std::unordered_map<key, std::pair<val, int>> _values;
+    public:
+        val get(key k, int time, std::function<val()> cb) {
+            auto it = _values.find(k);
+            if (it == _values.end()) {
+                val ret = cb();
+                _values.insert(std::make_pair(k, std::make_pair(ret, time + get_ttl())));
+                return ret;
+            }
+            if ((*it).second.second < time) {
+                _values.erase(it);
+                return get(k, time, cb);
+            }
+            return (*it).second.first;
+        }
+    };
 }
