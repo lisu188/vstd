6201c503f5701009eb329856b9494c4e7e5cc6a3
Andrzej Lis <andrzej.lis3@gmail.com>
2018-07-31 16:00:49 +0200

    tuple size

--- vtuple.h
+++ vtuple.h
@@ -3,11 +3,21 @@
 namespace vstd {
 
     template<std::size_t __i, typename _Head, typename... _Tail>
-    struct tuple_element : tuple_element<__i - 1, _Tail... > {
+    struct tuple_element : tuple_element<__i - 1, _Tail...> {
     };
 
     template<typename _Head, typename... _Tail>
-    struct tuple_element<0, _Head, _Tail... > {
+    struct tuple_element<0, _Head, _Tail...> {
         typedef _Head type;
+    };
+
+    template<typename _Head, typename... _Tail>
+    struct tuple_size {
+        size_t size = tuple_size<_Tail...>::size + 1;
+    };
+
+    template<typename _Head>
+    struct tuple_size<_Head> {
+        size_t size = 1;
     };
 }
