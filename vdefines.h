5c699ded8bdedc2c5ae627ab47bc96227c6199bd
Andrzej Lis <andrzej.lis3@gmail.com>
2019-04-13 20:15:29 +0200

    add autosemicolon to macros

--- vdefines.h
+++ vdefines.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #ifdef Py_PYTHON_H
-#define PY_SAFE(x) try{x}catch(...){vstd::logger::debug();PyErr_Print();PyErr_Clear();}
-#define PY_UNSAFE(x) try{x}catch(...){vstd::logger::debug();PyErr_Print();PyErr_Clear();throw;}
-#define PY_SAFE_RET(x) try{x}catch(...){vstd::logger::debug();PyErr_Print();PyErr_Clear();return nullptr;}
-#define PY_SAFE_RET_VAL(x, r) try{x}catch(...){vstd::logger::debug();PyErr_Print();PyErr_Clear();return r;}
+#define PY_SAFE(x) try{x;}catch(...){vstd::logger::debug();PyErr_Print();PyErr_Clear();}
+#define PY_UNSAFE(x) try{x;}catch(...){vstd::logger::debug();PyErr_Print();PyErr_Clear();throw;}
+#define PY_SAFE_RET(x) try{x;}catch(...){vstd::logger::debug();PyErr_Print();PyErr_Clear();return nullptr;}
+#define PY_SAFE_RET_VAL(x, r) try{x;}catch(...){vstd::logger::debug();PyErr_Print();PyErr_Clear();return r;}
 #endif
