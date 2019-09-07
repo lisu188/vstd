223e47ad407ffbd70aba64ce4c2f94675183c39a
Andrzej Lis <andrzej.lis3@gmail.com>
2019-09-07 21:52:41 +0200

    vmeta example

--- main.cpp
+++ main.cpp
@@ -11,6 +11,34 @@
  */
 #include "vstd.h"
 
+class CMetaExample {
+V_META(CMetaExample,
+       vstd::meta::empty,
+       V_PROPERTY(CMetaExample, std::string, text, getText, setText))
+public:
+    CMetaExample() = default;
+
+    std::string getText() {
+        return text;
+    }
+
+    void setText(std::string text) {
+        CMetaExample::text = text;
+    }
+
+private:
+    std::string text;
+};
+
+void vmeta_example() {
+    auto example = std::make_shared<CMetaExample>();
+    CMetaExample::static_meta()->set_property<CMetaExample, std::string>("text", example, "exampleText");
+    vstd::logger::info("Direct access:", example->getText());
+    vstd::logger::info("Meta access:",
+                       CMetaExample::static_meta()->get_property<CMetaExample, std::string>("text", example));
+}
+
 int main() {
+    vmeta_example();
     return 0;
 }
