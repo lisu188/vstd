3c2de1c4cfc2a93e68a7fb161d10cad857903799
Lis, Andrzej <andrzej.lis3@gmail.com>
2023-03-15 10:07:01 +0000

    vstd update

--- vstring.h
+++ vstring.h
@@ -44,13 +44,13 @@ namespace vstd {
 
     template<typename T=void>
     std::string ltrim(std::string s) {
-        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not_fn(std::ptr_fun<int, int>(std::isspace))));
         return s;
     }
 
     template<typename T=void>
     std::string rtrim(std::string s) {
-        s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+        s.erase(std::find_if(s.rbegin(), s.rend(), std::not_fn(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
         return s;
     }
 
